c20b468494a0a7004c8c9e0e869f73e7beb30db3
Rohan-avaloq <rohan.walunjkar@avaloq.com>
2024-11-28 15:42:19 +0530

    Added App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+// /src/App.js
 
-function App() {
+import React, { useState } from 'react';
+import PlayerList from './components/PlayerList';
+import Login from './components/Login';
+
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLoginSuccess = () => {
+    setIsLoggedIn(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {isLoggedIn ? (
+        <PlayerList />
+      ) : (
+        <Login onLoginSuccess={handleLoginSuccess} />
+      )}
     </div>
   );
-}
+};
 
 export default App;
